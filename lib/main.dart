a4d8bfeb11cedc687687c22828ac99b492f5e367
UK-Dev786 <ukdev.codetivelab@gmail.com>
2025-06-18 16:20:54 +0500

    connect the project with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   print("App starting...");
   runApp(
     GetMaterialApp(
